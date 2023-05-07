89e66f8320f1eb1a5625e336d16f95d3091c70be
RohanSaraswat <73846514+RohanSaraswat@users.noreply.github.com>
2023-05-07 22:58:53 +0530

    .

---
+++ Dynamic Programming/LIS.cpp
@@ -0,0 +1,21 @@
+// TC O(N)
+// SC O(N)
+
+int lengthOfLIS(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> dp(n, 1);
+    int len = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (nums[j] < nums[i] and dp[j] + 1 > dp[i])
+            {
+                dp[i] = dp[j] + 1;
+            }
+        }
+        len = max(len, dp[i]);
+    }
+    return len;
+}
