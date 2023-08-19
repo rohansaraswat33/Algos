83951cbc3775f774a4ab7677219577dd25272380
Rohan Saraswat <rohansaraswat33@gmail.com>
2023-08-19 18:47:34 +0530

    Better version of DSU

--- Graph/DSU.cpp
+++ Graph/DSU.cpp
@@ -4,39 +4,48 @@ using namespace std;
 // TC of both union & find op: O(4*alpha)~O(4)
 // SC : O(n)
 
-class DSU
+class UnionFind
 {
 public:
-    vector<int> parent, rank;
-    DSU(int n)
+    vector<int> parent;
+    vector<int> size;
+    int maxSize;
+
+    UnionFind(int n)
     {
-        parent.resize(n, 0);
-        rank.resize(n, 0);
+        parent.resize(n);
+        size.resize(n, 1);
+        maxSize = 1;
         for (int i = 0; i < n; i++)
-            parent[i] = i;
-    }
-    int find(int u)
-    {
-        if (parent[u] == u)
-            return u;
-        return parent[u] = find(parent[u]);
-    }
-    bool unionxy(int u, int v)
-    {
-        int parU = find(u);
-        int parV = find(v);
-        if (parU != parV)
         {
-            if (rank[parU] == rank[parV])
-                rank[parU]++;
-            if (rank[parU] < rank[parV])
+            parent[i] = i;
+        }
+    }
+
+    int find(int x)
+    {
+        // Finds the root of x
+        if (x != parent[x])
+        {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    bool unite(int x, int y)
+    {
+        // Connects x and y
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY)
+        {
+            if (size[rootX] < size[rootY])
             {
-                parent[parU] = parV;
+                swap(rootX, rootY);
             }
-            else
-            {
-                parent[parV] = parU;
-            }
+            parent[rootY] = rootX;
+            size[rootX] += size[rootY];
+            maxSize = max(maxSize, size[rootX]);
             return true;
         }
         return false;
